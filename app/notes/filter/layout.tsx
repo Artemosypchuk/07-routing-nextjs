7cd729395bf70e2239820d53b88b8f598e1d1d62
tyoska <tyoska111@ukr.net>
2026-08-09 14:02:58 +0300

    Small fix

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -2,12 +2,10 @@ import css from "./Layout.module.css";
 interface SidebarProps {
   children: React.ReactNode;
   sidebar: React.ReactNode;
-  modal: React.ReactNode;
 }
 export default function SidebarNotesLayout({
   children,
   sidebar,
-  modal,
 }: SidebarProps) {
   return (
     <section className={css.container}>
